9a09b3a88b42287bade76754f66a71aa97037185
Dusty McCord <Dustymccord@gmail.com>
2020-05-25 14:11:27 -0700

    initial commit.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  return console.log(`Server started on port ${PORT}`);
+});
