bf3225127e2cdbcc169dfbb8493fb779c2c6492a
Dusty McCord <Dustymccord@gmail.com>
2020-05-26 09:36:51 -0700

    update auth.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -33,7 +33,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: 'error' });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
