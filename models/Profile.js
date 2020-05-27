413732336e70077ee18f2cbce944ae3c266b4ceb
Dusty McCord <Dustymccord@gmail.com>
2020-05-26 23:14:41 -0700

    add profile for a signed in user.

---
+++ models/Profile.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+  },
+  webiste: {
+    type: String,
+  },
+  locaiton: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        require: true,
+      },
+      degree: {
+        type: String,
+        require: true,
+      },
+      fieldofstudy: {
+        type: String,
+        require: true,
+      },
+      from: {
+        type: String,
+        require: true,
+      },
+      to: {
+        type: String,
+      },
+      current: {
+        type: String,
+        require: true,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkeding: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
