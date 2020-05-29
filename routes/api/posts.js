56caf7c436b3de814964aa049187b4acd96afcd0
Dusty McCord <Dustymccord@gmail.com>
2020-05-29 09:21:41 -0700

    add Post Schema and add post.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,42 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/posts
-// @Desc    Test route
-// @access  Public
+// @route   POST api/posts
+// @Desc    Create a post
+// @access  private
 
-router.get("/", (req, res) => res.send("Posts route"));
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required ').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
