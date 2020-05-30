67f87896f5352d62ce0eab80c1d1864557578d41
Dusty McCord <Dustymccord@gmail.com>
2020-05-29 22:00:11 -0700

    add react front end.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+
+export default App;
