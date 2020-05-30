f7a8226bdf38c3e47ef1752b4374cd1996dd4c8a
Dusty McCord <Dustymccord@gmail.com>
2020-05-30 12:57:09 -0700

    wire alert reducer, action, and component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './Store';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Route exact path='/' component={Landing} />
-    <section className='container'>
-      <Switch>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-      </Switch>
-    </section>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Alert />
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-function Register() {
+function Register({ setAlert }) {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,7 +21,7 @@ function Register() {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
       const newUser = {
         name,
@@ -102,4 +105,8 @@ function Register() {
   );
 }
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
