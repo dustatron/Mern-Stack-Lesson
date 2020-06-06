e6c2adfe4f07fd5230c3515727294cef49eb6e34
Dusty McCord <Dustymccord@gmail.com>
2020-06-05 19:36:52 -0700

    add comments to a post.

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,6 +7,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from './types';
 
 // Get posts
@@ -58,6 +60,7 @@ export const removeLike = (id) => async (dispatch) => {
     });
   }
 };
+
 // Delete Post
 export const deletePost = (id) => async (dispatch) => {
   try {
@@ -106,6 +109,46 @@ export const getPost = (id) => async (dispatch) => {
         msg: err.response.statusText,
         status: err.response.status,
       },
+    });
+  }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+
+    dispatch({ type: ADD_COMMENT, payload: res.data });
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    console.log('error', err);
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({ type: REMOVE_COMMENT, payload: commentId });
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    console.log('error', err);
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
 };
